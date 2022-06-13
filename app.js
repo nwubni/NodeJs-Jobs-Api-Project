9dd346749ad71f9527c336933635a9ea3c15aa9b
Nakuru Wubni <nwubni@gmail.com>
2022-06-13 08:48:57 +0100

    Added Swagger documentation

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
-//Extra security
+//Extra security packages
 const helmet = require('helmet');
 const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
+
+//Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDoc = YAML.load('./swagger.yaml');
 
 const connectDB = require('./db/connect');
 const authMiddleware = require('./middleware/authentication');
@@ -22,10 +27,13 @@ app.use(express.json());
 app.set('trust proxy', 1);
 app.use(rateLimiter({ windowMs: 60 * 1000, max: 60 }));
 app.use([helmet(), cors(), xss()]);
+
 // extra packages
 app.get('/', (req, res) => {
-    res.send('Welcome to the jobs API service.');
+    res.send('<h2>Welcome to the jobs API service.</h1><a href="/api-docs">View the API documentation</a>');
 });
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
 app.use('/api/v1/auth', auth_router);
 app.use('/api/v1/jobs', authMiddleware, jobs_router);
